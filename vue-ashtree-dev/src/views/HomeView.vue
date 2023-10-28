9187d24409205d401822e7e9405e4fe641eda31f
Ashton Martinez <martinezashton03@gmail.com>
2023-10-28 17:25:58 -0400

    Update Home page style

--- vue-ashtree-dev/src/views/HomeView.vue
+++ vue-ashtree-dev/src/views/HomeView.vue
@@ -5,8 +5,8 @@
 
       <NavBar/>
 
-      <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="bg-white w-50 p-5 rounded">
+      <div class="d-flex justify-content-center align-items-center vh-100 ">
+        <div class="w-50 p-5 rounded text-box">
           <p class="main-text">
             Hi! i'm Ashton Martinez, a dedicated freelance game and web developer. My journey in the digital domain spans Both passion projects and professional endeavors, with a strong focus on delivering high-quality results in every task
           </p>
@@ -51,6 +51,11 @@ export default defineComponent({
   font-size: 27px;
   letter-spacing: 0.7px;
   line-height: 43px;
+  color: rgb(181, 186, 190);
+}
+
+.text-box {
+  background-color: rgb(78, 83, 97);
 }
 
 </style>
