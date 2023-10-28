2267f4b6309f49adcffef26283fb17345880da4e
Ashton Martinez <martinezashton03@gmail.com>
2023-10-28 17:21:26 -0400

    Initial commit

---
+++ vue-ashtree-dev/src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="NavBar fixed-top d-flex justify-content-between align-items-center p-3">
+    <div>
+        <h1 class="m-3 text-white">Ashtree Dev</h1>
+    </div>
+    <div class="d-flex">
+        <RouterLink class="p-3 h2 nav-item" to="/">Home</RouterLink>
+        <RouterLink class="p-3 h2 nav-item" to="/">Game Dev</RouterLink>
+        <RouterLink class="p-3 h2 nav-item" to="/">Web Dev</RouterLink>
+    </div>
+</div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+name: 'Navbar',
+
+data() {
+    return {
+    
+    };
+},
+methods: {
+    
+},
+});
+</script>
+
+<style scoped>
+
+.nav-item {
+    text-align: center;
+    color: rgb(222, 230, 230);
+    font-weight: 200;
+    text-decoration: none;
+}
+
+.NavBar {
+    background-color: rgb(38, 39, 53);
+}
+
+</style>
+  
---
+++ vue-ashtree-dev/src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+  <body>
+    <div>
+    <div class="home-content">
+
+      <NavBar/>
+
+      <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="bg-white w-50 p-5 rounded">
+          <p class="main-text">
+            Hi! i'm Ashton Martinez, a dedicated freelance game and web developer. My journey in the digital domain spans Both passion projects and professional endeavors, with a strong focus on delivering high-quality results in every task
+          </p>
+
+          <p class="main-text">If you're seeking a developer with a balance of creativity, technical expertise, and professionalism, I invite you to explore my portfolio or get in touch directly.
+</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  </body>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavBar from '@/components/NavBar.vue'; // Adjust the import path based on your project structure
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    NavBar, // Register your NavBar component here
+  },
+
+  data() {
+    return {
+      // Your data properties here
+    };
+  },
+
+  methods: {
+    // Your methods here
+  },
+});
+</script>
+
+<style scoped>
+
+.main-text {
+  font-size: 27px;
+  letter-spacing: 0.7px;
+  line-height: 43px;
+}
+
+</style>
